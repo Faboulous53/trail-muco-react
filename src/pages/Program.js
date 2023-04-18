1894bfb9ee50d1d100b053b467437f5df0d67a01
Fabien Delpierre <fab.delpierre@gmail.com>
2023-04-18 14:16:50 +0200

    creation des pages (composants/style) en fonction du style WP)

--- src/pages/Program.js
+++ src/pages/Program.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const App = () => {
+const Program = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const App = () => {
     );
 };
 
-export default App;
+export default Program;
